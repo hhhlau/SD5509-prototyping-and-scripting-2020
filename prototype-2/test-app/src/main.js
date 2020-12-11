98cd7fdf6ea9af1fb3edc00aad5dd3a885a40a30
Hebitz Lau <hebitz.lau@qbssystem.com>
2020-12-11 14:42:12 +0800

    updated with mqtts

--- prototype-2/test-app/src/main.js
+++ prototype-2/test-app/src/main.js
@@ -4,7 +4,7 @@ import VueMqtt from 'vue-mqtt';
 import App from './App.vue'
 
 Vue.config.productionTip = false
-Vue.use(VueMqtt, 'mqtt://b9155527:881fabd58c0f4d8e@broker.shiftr.io', options);
+Vue.use(VueMqtt, 'mqtts://b9155527:881fabd58c0f4d8e@broker.shiftr.io', options);
 
 new Vue({
   render: h => h(App),
